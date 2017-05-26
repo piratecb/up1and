a0210e5049be0197a3d0c13607eb586a5f621fca
piratecb <piratecb@gmail.com>
2017-05-26 22:11:16 +0800

    fix permission decorator bugs

--- app/utils.py
+++ app/utils.py
@@ -1,16 +1,16 @@
 from functools import wraps
 from flask import abort
-from flask.ext.login import current_user
+from flask_login import current_user
 from .models import Permission
 
 
 def permission_required(permission):
     def decorator(func):
-        @wraps(f)
+        @wraps(func)
         def wrapper(*args, **kwargs):
             if not current_user.can(permission):
                 abort(403)
-            return f(*args, **kwargs)
+            return func(*args, **kwargs)
         return wrapper
     return decorator
 
