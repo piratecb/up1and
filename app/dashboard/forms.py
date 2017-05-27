8e5c5d0afb88cfea5d767e67a77404bc18d9586a
piratecb <piratecb@gmail.com>
2017-05-27 10:53:46 +0800

    add user profile view

--- app/dashboard/forms.py
+++ app/dashboard/forms.py
@@ -1,16 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectMultipleField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectMultipleField, SelectField
 from wtforms.validators import Required, Length, EqualTo, Email, DataRequired
 from wtforms import ValidationError
-from ..models import Meta
+from ..models import Meta, User
 
 
-class UserEditForm(FlaskForm):
-    password = PasswordField('密码', validators=[EqualTo('password_confirm', message='密码必须相等')])
-    password_confirm = PasswordField('确认密码')
+class ProfileEditForm(FlaskForm):
     nickname = StringField('昵称', validators=[Length(1, 64)])
     email = StringField('电子邮箱', validators=[Length(1, 64), Email()])
-    role = BooleanField('管理员')
+    submit = SubmitField('更新我的档案')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已被使用')
+
+
+class ChangePasswordForm(FlaskForm):
+    password = PasswordField('密码', validators=[Required(), EqualTo('password_confirm', message='密码必须相等')])
+    password_confirm = PasswordField('确认密码', validators=[Required()])
+    submit = SubmitField('更新密码')
+
+
+class UserEditForm(ChangePasswordForm, ProfileEditForm):
+    GROUPS = [(key, key.capitalize()) for key in User.PERMISSIONS]
+
+    group = SelectField('用户组', choices=GROUPS)
     submit = SubmitField('更新')
 
 
--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,14 +1,18 @@
 from flask import render_template, redirect, request, url_for, flash, current_app, abort
 from flask_login import login_required, current_user
 from . import dashboard
-from .forms import UserEditForm, PostForm
+from .forms import UserEditForm, PostForm, ProfileEditForm, ChangePasswordForm
 from .. import db
-from ..models import Post
+from ..models import Post, Permission
+from ..utils import permission_required, admin_required
 
 
 @dashboard.route('/')
 @login_required
 def index():
+    if current_user.group == 'user':
+        return redirect(url_for('dashboard.profile'))
+
     posts = Post.query.filter_by(type='post').order_by(Post.created.desc())
     return render_template('dashboard/index.html', posts=posts)
 
@@ -66,30 +70,46 @@ def manage_posts():
     return render_template('dashboard/manage_posts.html', posts=posts, pagination=pagination)
 
 
-
 @dashboard.route('/write-page')
 @login_required
 def write_page():
     return render_template('dashboard/edit_page.html')
 
 
+@dashboard.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    profile_form = ProfileEditForm(prefix="profile_form")
+    password_form = ChangePasswordForm(prefix="password_form")
+
+    if profile_form.validate_on_submit() and profile_form.submit.data:
+        print('profile')
+        return redirect(url_for('dashboard.profile'))
+
+    if password_form.validate_on_submit() and password_form.submit.data:
+        print('password')
+        return redirect(url_for('dashboard.profile'))
+
+    return render_template('dashboard/profile.html', profile_form=profile_form, password_form=password_form)
+
+
 @dashboard.route('/user', methods=['GET', 'POST'])
 @login_required
+@admin_required
 def user():
     form = UserEditForm()
     if form.validate_on_submit():
-        # print(request.form['email'])
         if form.password.data:
             current_user.password = form.password.data
             flash('密码修改成功')
         current_user.email = form.email.data
         current_user.nickname = form.nickname.data
-        current_user.role = form.role.data
+        current_user.group = form.group.data
         db.session.add(current_user)
         flash('用户资料更新成功')
         return redirect(url_for('dashboard.user'))
     form.email.data = current_user.email
     form.nickname.data = current_user.nickname
-    form.role.data = current_user.role
+    form.group.data = current_user.group
     return render_template('dashboard/user.html', form=form)
 
