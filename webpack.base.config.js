82b7fb18554acc1acfd5241c62e5c013fabd38ee
up1and <piratecb@gmail.com>
2019-05-07 21:52:17 +0800

    add less loader in webpack

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -25,6 +25,10 @@ function createConfig(entry, outpath) {
           use: ['babel-loader']
         },
         {
+          test: /\.less$/,
+          use: ['style-loader', 'css-loader', 'less-loader']
+        },
+        {
           test: /\.css$/,
           use: [MiniCssExtractPlugin.loader, 'css-loader']
         },
